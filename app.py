2281be8049278e86745ee485ac70ff3462f04ebb
Utkarsh Tripathy <97348517+Ut14@users.noreply.github.com>
2025-06-26 00:15:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from fastapi import FastAPI, File, UploadFile, Request, Form
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
 from PIL import Image
 import torch
 import numpy as np
@@ -9,14 +8,25 @@ import joblib
 from transformers import CLIPProcessor, CLIPModel
 import io
 import os
+from dotenv import load_dotenv
+
+# --- Load environment variables ---
+load_dotenv()
+HF_TOKEN = os.getenv("HF_TOKEN")
 
 # --- Init FastAPI app and template dir ---
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 # --- Load CLIP and SVM model once ---
-clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-clip_processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+clip_model = CLIPModel.from_pretrained(
+    "Ut14/clip-phone-view", subfolder="clip_model", token=HF_TOKEN
+)
+clip_processor = CLIPProcessor.from_pretrained(
+    "Ut14/clip-phone-view", subfolder="clip_processor", token=HF_TOKEN
+)
+
+# --- Load SVM Model ---
 svm_model = joblib.load("svm_phone_view_model.joblib")
 label_map = {0: "front", 1: "back", 2: "side"}
 
